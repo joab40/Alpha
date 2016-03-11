e29cab64cb0ba18870c0e459022beb4ea85720fb
joab40 <tornvagen@gmail.com>
2016-03-11 01:08:04 +0100

    Added GPIO Raspberry LED support

--- bin/alpha.py
+++ bin/alpha.py
@@ -6,6 +6,7 @@ import speech_recognition as sr
 import pyvona
 import argparse
 import yaml
+import time
 import logging
 from commands import *
 
@@ -20,7 +21,9 @@ libpath = basepath + "/../lib/"
 
 if libpath not in sys.path:
         sys.path.insert(1, libpath)
-
+print "test"
+import led_class
+print "test2"
 def readyaml(yfile):
     logger.info("readyaml             - read yaml file            [\033[0;32mREAD\033[0m] on admin on host: %s ", yfile)
     # Add check for file exist/read
@@ -29,13 +32,15 @@ def readyaml(yfile):
     f.close()
     return config
 
-def run_sasha(onlytextoption):
+def run_sasha(onlytextoption,onlytexttospeech):
 
     while True:
         # obtain audio from the microphone
         message = "EMTPY"
-        if onlytextoption:
+        if onlytextoption or onlytexttospeech:
+            piled.on('green')
             message = raw_input("Talk To Alpha: ")
+            piled.off()
 
         else:
             r = sr.Recognizer()
@@ -53,17 +58,23 @@ def run_sasha(onlytextoption):
                 # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
                 # instead of `r.recognize_google(audio)`
                 message = r.recognize_google(audio,language="sv-SE")
+                piled.on('green')
+                piled.off()
                 print("Google Speech Recognition thinks you said " + message)
                 logger.info("sasha think you said  - google speak              [\033[0;32mOK\033[0m] : %s", message)
             except sr.UnknownValueError:
+                piled.on('red')
+                piled.off()
                 #print("Google Speech Recognition could not understand audio")
                 logger.info("sasha Google SpeechRecognition failed understand  [\033[0;31mFAILED\033[0m] : %s", message)
                 #logger.info("sasha think you said  - google speak              [\033[0;32mOK\033[0m] : %s", message)
             except sr.RequestError as e:
                 print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
+                piled.on('red')
+                piled.off()
 
         if message == "avsluta":
+            piled.off()
             exit()
         elif message == "save":
             kernel.saveBrain("bot_brain.brn")
@@ -84,58 +95,69 @@ def run_sasha(onlytextoption):
                         print "failed: ",status
 
                 if onlytextoption:
-                    print "Alpha says: ", bot_response
+                        piled.on('yellow')
+                        print "Alpha says: ", bot_response
+                        time.sleep(0.5)
+                        piled.off()
                 else:
-                    #os.system(ESPEAK + '"' + bot_response + '"')
-                    v.speak(bot_response)
-            # Do something with bot_response
+                        #os.system(ESPEAK + '"' + bot_response + '"')
+                        piled.on('yellow')
+                        v.speak(bot_response)
+                        time.sleep(0.5)
+                        piled.off()
+                # Do something with bot_response
 
 
 if __name__ == '__main__':
-    Version = "0.01"
-    parser = argparse.ArgumentParser(description='sasha')
-    parser.add_argument("--textonly", "-t", help="Text only, disable all voice", action="store_true")
-    parser.add_argument('-d', '--loglevel',default='', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL' ], help='Default WARNING')
-    parser.add_argument('-f', '--logfile', default='', help='Path to Logfile')
-    args = parser.parse_args()
+        Version = "0.01"
+        parser = argparse.ArgumentParser(description='sasha')
+        parser.add_argument("--textonly", "-t", help="Text only, disable all voice", action="store_true")
+        parser.add_argument("--ttsonly", "-s", help="Text only, disable all voice", action="store_true")
+        parser.add_argument('-d', '--loglevel',default='', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL' ], help='Default WARNING')
+        parser.add_argument('-f', '--logfile', default='', help='Path to Logfile')
+        args = parser.parse_args()
+        print "test3"
+        if args.logfile:
+            numeric_level = getattr(logging, args.loglevel.upper(), None)
+            FORMAT = '%(asctime)-15s %(levelname)s    - %(name)s - %(message)s'
+            logging.basicConfig(format=FORMAT,level=numeric_level,filename=args.logfile)
+            logger = logging.getLogger('Islay Of Mist')
+        else:
+            numeric_level = getattr(logging, args.loglevel.upper(), None)
+            FORMAT = '%(asctime)-15s %(levelname)s    - %(name)s - %(message)s'
+            logging.basicConfig(format=FORMAT,level=numeric_level)
+            logger = logging.getLogger('Islay Of Mist')
+        print "test4"
+        logger.debug("__main__: Starting ervers: %s ",Version)
 
-    if args.logfile:
-        numeric_level = getattr(logging, args.loglevel.upper(), None)
-        FORMAT = '%(asctime)-15s %(levelname)s    - %(name)s - %(message)s'
-        logging.basicConfig(format=FORMAT,level=numeric_level,filename=args.logfile)
-        logger = logging.getLogger('Islay Of Mist')
-    else:
-        numeric_level = getattr(logging, args.loglevel.upper(), None)
-        FORMAT = '%(asctime)-15s %(levelname)s    - %(name)s - %(message)s'
-        logging.basicConfig(format=FORMAT,level=numeric_level)
-        logger = logging.getLogger('Islay Of Mist')
+        print "faan"
+        try:
+            # Read Configurations yaml config file
+            yconfig = readyaml(ymlfile)
+        except:
+            print "Couldnt find ymlfile !?", ymlfile
+            sys.exit(1)
 
-    logger.debug("__main__: Starting ervers: %s ",Version)
-
-    try:
-        # Read Configurations yaml config file
-        yconfig = readyaml(ymlfile)
-    except:
-        print "Couldnt find ymlfile !?", ymlfile
-        sys.exit(1)
-
-    logger.info("main                 - yaml read                 [\033[0;32mOK\033[0m] %s " ,yconfig['pyvonna']['user_key'])
-    logger.info("main                 - yaml read                 [\033[0;32mOK\033[0m] %s " ,yconfig['pyvonna']['cert_key'])
-
-
-    # Create the kernel and learn AIML files
-    kernel = aiml.Kernel()
-    kernel.learn("../etc/std-startup.xml")
-    kernel.respond("load aiml b")
-
-    ESPEAK = "/usr/bin/espeak -v swedish "
-    ONTELLSTICKTEST = "ssh life " + "'" + "/usr/bin/tdtool --on vardagsrum" + "'"
-    OFFTELLSTICKTEST = "ssh life " + "'" + "/usr/bin/tdtool --off vardagsrum" + "'"
-    TELLSTICKTEMP =  "ssh life /usr/local/bin/temperatur.sh"
-
-    if not args.textonly:
-        v = pyvona.create_voice(yconfig['pyvonna']['user_key'], yconfig['pyvonna']['cert_key'])
-        v.voice_name = 'Astrid'
+        logger.info("main                 - yaml read                 [\033[0;32mOK\033[0m] %s " ,yconfig['pyvonna']['user_key'])
+        logger.info("main                 - yaml read                 [\033[0;32mOK\033[0m] %s " ,yconfig['pyvonna']['cert_key'])
 
 
-    run_sasha(args.textonly)
+        # Create the kernel and learn AIML files
+        kernel = aiml.Kernel()
+        kernel.learn("../etc/std-startup.xml")
+        kernel.respond("load aiml b")
+
+        ESPEAK = "/usr/bin/espeak -v swedish "
+        ONTELLSTICKTEST = "ssh life " + "'" + "/usr/bin/tdtool --on vardagsrum" + "'"
+        OFFTELLSTICKTEST = "ssh life " + "'" + "/usr/bin/tdtool --off vardagsrum" + "'"
+        TELLSTICKTEMP =  "ssh life /usr/local/bin/temperatur.sh"
+
+        if not args.textonly:
+            v = pyvona.create_voice(yconfig['pyvonna']['user_key'], yconfig['pyvonna']['cert_key'])
+            v.voice_name = 'Astrid'
+        piled = led_class.led()
+        #piled.on('blue')
+        #time.sleep(1)
+        #piled.off()
+
+        run_sasha(args.textonly,args.ttsonly)
