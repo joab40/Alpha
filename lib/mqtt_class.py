2b8f5af2eb805b5f0656a368d6d064ce1a0e7835
joab40 <tornvagen@gmail.com>
2016-03-20 00:42:32 +0100

    added mqtt class

---
+++ lib/mqtt_class.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+import os
+import paho.mqtt.client as mqtt
+import json
+
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+
+class alphamqtt(object):
+    def __init__(self,broker,brokerport, topic):
+        # creating object
+        self.topic = topic
+        self.broker = broker
+        self.brokerport = brokerport
+        self.count = 0
+        self.client2 = mqtt.Client()
+        self.client2.connect(broker, brokerport, 60)
+
+    def pub(self,amsg):
+        self.count +=1
+        message = json.dumps({'xcord': amsg, 'id': self.count, 'time': time.time()})
+        print message
+        self.client2.publish(self.topic, message)
+
+    def sub(self):
+        print "subscribe"
+
+
+
+
+
+
+test = alphamqtt('localhost','1883','alphahead')
+test.pub('333')
+#time.sleep(1)
+#test.off()
+# test.off()
+# test.blink_on('red',"1")
--- test/camera_read_motion_log.py
+++ test/camera_read_motion_log.py
@@ -1,7 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import subprocess
+import os
+import sys
+
+basepath = os.path.dirname(os.path.abspath(__file__))
+ymlfile = basepath + "/../etc/alpha_config.yml"
+libpath = basepath + "/../lib/"
+
+if libpath not in sys.path:
+        sys.path.insert(1, libpath)
+
+try:
+        import mqtt_class
+except ImportError:
+        print 'You need mqtt_class in %s ',libpath
+        sys.exit(1)
+
+
+sender = mqtt_class.alphamqtt('localhost','1883','alphahead')
 filename = '/tmp/motion.log'
 f = subprocess.Popen(['tail','-F',filename],\
         stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 while True:
     line = f.stdout.readline()
-    print line
+    if 'jpg' in line:
+        #line.split()
+        line = line.replace('jpg','')
+        line = line.replace('.','')
+        line = line.replace('-',' ')
+        line = line.rsplit(None, 1)[-1]
+        sender.pub(line)
+        print line
