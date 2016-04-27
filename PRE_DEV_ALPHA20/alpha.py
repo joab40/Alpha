f8eb317d505e5c8b3f2e4e0ece31e509cd1fad0e
joab40 <tornvagen@gmail.com>
2016-04-28 01:22:56 +0200

    new alpha.py template PRE ALPHA20

---
+++ PRE_DEV_ALPHA20/alpha.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import aiml
+import os
+import speech_recognition as sr
+import pyvona
+import argparse
+import yaml
+import time
+import logging
+from commands import *
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+basepath = os.path.dirname(os.path.abspath(__file__))
+ymlfile = basepath + "/../etc/alpha_config.yml"
+libpath = basepath + "/../lib/"
+
+def readyaml(yfile):
+    logger.info("readyaml             - read yaml file            [\033[0;32mREAD\033[0m] on admin on host: %s ", yfile)
+    # Add check for file exist/read
+    with open(yfile, 'r') as f:
+        config =  yaml.load(f)
+    f.close()
+    return config
+
+if __name__ == '__main__':
+        Version = "2.00"
+        parser = argparse.ArgumentParser(description='sasha')
+        parser.add_argument("--textonly", "-t", help="Text only, disable all voice", action="store_true")
+        parser.add_argument("--ttsonly", "-s", help="Text only, disable all voice", action="store_true")
+        parser.add_argument('-d', '--loglevel',default='', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL' ], help='Default WARNING')
+        parser.add_argument('-f', '--logfile', default='', help='Path to Logfile')
+        args = parser.parse_args()
+        if args.logfile:
+            numeric_level = getattr(logging, args.loglevel.upper(), None)
+            FORMAT = '%(asctime)-15s %(levelname)s    - %(name)s - %(message)s'
+            logging.basicConfig(format=FORMAT,level=numeric_level,filename=args.logfile)
+            logger = logging.getLogger('Islay Of Mist')
+        else:
+            numeric_level = getattr(logging, args.loglevel.upper(), None)
+            FORMAT = '%(asctime)-15s %(levelname)s    - %(name)s - %(message)s'
+            logging.basicConfig(format=FORMAT,level=numeric_level)
+            logger = logging.getLogger('Islay Of Mist')
+        logger.debug("__main__: Starting ervers: %s ",Version)
+
+        try:
+            # Read Configurations yaml config file
+            yconfig = readyaml(ymlfile)
+        except:
+            print "Couldnt find ymlfile !?", ymlfile
+            sys.exit(1)
+
+
+        if yconfig['camera']['enable']:
+           print "Subscribe to camera"
+        
